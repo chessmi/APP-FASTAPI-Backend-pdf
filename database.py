1cf3e9cd179812216b5eb27a31117b8f82c6b761
JoseMiguelCantoCrespo <josemiguelcantocrespo@gmail.com>
2025-05-13 06:19:42 +0200

    first commit code

---
+++ database.py
@@ -0,0 +1,28 @@
+import os
+from sqlalchemy import create_engine
+#from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData
+from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DATABASE_USER']}:@{os.environ['DATABASE_HOST']}/{os.environ['DATABASE_NAME']}"
+
+user = os.environ['DATABASE_USER']
+password = os.environ['DATABASE_PASSWORD']
+host = os.environ['DATABASE_HOST']
+port = os.environ['DATABASE_PORT']
+db_name = os.environ['DATABASE_NAME']
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#Base = declarative_base()
+metadata = MetaData(schema="fastapi_schema")
+Base = declarative_base(metadata=metadata)
